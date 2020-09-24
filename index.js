f3bdf47ebef0f7534785377e10916aa14200a5b0
Muralidevan <muralivj.p@gmail.com>
2020-09-24 15:44:28 +0530

    prepared for deployment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const connectDB = require('./config/database')
 
 const router = require('./config/routes')
+const path = require('path')
 
 const app = express()
 
@@ -17,7 +18,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(router)
 app.use('/public', express.static('public'))
 
-app.get('/', (req, res) => res.send('API Running'))
+//serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 3055
 
 app.listen(PORT, () => {
