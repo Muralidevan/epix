060320028957083a8d5947a39e29af13e21569a0
Muralidevan <muralivj.p@gmail.com>
2020-09-25 16:05:16 +0530

    new fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/public', express.static('public'))
 //serve static assests in production
 if (process.env.NODE_ENV === 'production') {
 	//Set static folder
-	app.use(express.static(paht.join(__dirname, 'client/build')))
+	app.use(express.static(path.join(__dirname, 'client/build')))
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, +'/client/build/index.html'))
